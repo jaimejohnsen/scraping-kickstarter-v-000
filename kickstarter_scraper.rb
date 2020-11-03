f01fac8a325f2cb621b7494bf64104e9f12cb818
Jaime Johnsen <drivetraindiva@gmail.com>
2020-11-03 16:24:46 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -30,4 +30,3 @@ end
  # description: project.css("p.bbcard_blurb").text
  #project.css("ul.project-meta").text.strip
  #project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-
