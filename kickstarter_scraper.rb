1bcbc0d8203ab995d6ec5664d9e7165293febe59
Jaime Johnsen <drivetraindiva@gmail.com>
2020-11-03 15:55:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,9 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+
+  projects = {}
+  
 end
 
  #projects: kickstarter.css("li.project.grid_4")
